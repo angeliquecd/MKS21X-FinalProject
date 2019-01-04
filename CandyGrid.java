081b30386852a38ad3dad68779f94b9bf254c278
angeliquecd <acharles-davis00@stuy.edu>
2019-01-04 10:08:31 -0500

    wrote basic constructor and fields

--- CandyGrid.java
+++ CandyGrid.java
@@ -1,3 +1,24 @@
+import java.util.*;
+import java.io.*;
 public class CandyGrid{
-  int[][] candyGrid;
+  private Candy[][] candyGrid;
+  private int seed ;
+  private Random randgen;
+  private int row;
+  private int col;
+  public CandyGrid(){
+      Random seedgen= new Random();  //will alter later so that user can input a seed instead of one being generated
+      seed=seedgen.nextInt();
+      Random randgen= new Random(seed);
+      row=10;
+      col=10;//for now start at ten, will change later for levels
+      candyGrid=new Candy[row][col];
+      for (int a=0;a<row;a++){
+        for (int b=0;b<col;b++){
+          int color= randgen.nextInt();
+          candyGrid[a][b]=new Candy(color, false,false);
+        }
+      }
+  }
+
 }
