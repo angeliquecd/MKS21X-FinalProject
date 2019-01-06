9de589c0415fa5d747e3d9c642274efad36aa632
aditihaiman <ahaiman00@stuy.edu>
2019-01-06 11:04:53 -0500

    Wrote toStringDebug

--- CandyGrid.java
+++ CandyGrid.java
@@ -6,6 +6,12 @@ public class CandyGrid{
   private Random randgen;
   private int row;
   private int col;
+
+  public static void main(String[] args) {
+    CandyGrid cg = new CandyGrid();
+    System.out.println(cg.toStringDebug());
+  }
+
   public CandyGrid(){
       Random seedgen= new Random();  //will alter later so that user can input a seed instead of one being generated
       seed=seedgen.nextInt();
@@ -15,55 +21,72 @@ public class CandyGrid{
       candyGrid=new Candy[row][col];
       for (int a=0;a<row;a++){
         for (int b=0;b<col;b++){
-          int color= randgen.nextInt();
+          int color= randgen.nextInt(5);
           candyGrid[a][b]=new Candy(color, false,false);
         }
       }
   }
-public ArrayList<Integer> checkMatches(){
-  checkRows();
-}
-public ArrayList<Integer> checkRows(){
-  int currentcolor;
-  int candycolor;
-  int inarow;
-  ArrayList<Integer> toreturn = new ArrayList<Integer>();
-  for (int a=0;a<row;a++){
-    currentcolor=0;
-    candycolor=0;
-    inarow=0;
-    for (int b=0;b<col;b++){
-      candycolor=candyGrid[a][b].getColorInt();
-      if (candycolor!=currentcolor) {
-        currentcolor=candycolor;
-        if (inarow>=3){
-          toreturn.add(b);}
-        inarow=0;}
-      else{
-        inarow++;}
-    }
-  }
-  return toreturn;}
 
-public ArrayList<integer> checkCols(){
-  int currentcolor;
-  int candycolor;
-  int inarow;
-  ArrayList<Integer> toreturn = new ArrayList<Integer>();
-  for (int a=0;a<row;a++){
-    currentcolor=0;
-    candycolor=0;
-    inarow=0;
-    for (int b=0;b<col;b++){
-      candycolor=candyGrid[b][a].getColor();
-      if (candycolor!=currentcolor) {
-        currentcolor=candycolor;
-        if (inarow>=3){
-          toreturn.add(b);}
-        inarow=0;}
-      else{
-        inarow++;}
+  public String toStringDebug() {
+    String output = "";
+    for (int a=0; a<row; a++){
+      for (int b=0; b<col; b++){
+        output += " O ";
+      }
+      output += "\n";
     }
+    return output;
   }
-  return toreturn;}
+
+
+   public ArrayList<Integer> checkMatches(){
+     checkRows();
+   }
+
+  public ArrayList<Integer> checkRows(){
+    int currentcolor;
+    int candycolor;
+    int inarow;
+    ArrayList<Integer> toreturn = new ArrayList<Integer>();
+    for (int a=0;a<row;a++){
+      currentcolor=0;
+      candycolor=0;
+      inarow=0;
+      for (int b=0;b<col;b++){
+        candycolor=candyGrid[a][b].getColorInt();
+        if (candycolor!=currentcolor) {
+          currentcolor=candycolor;
+          if (inarow>=3){
+            toreturn.add(b);}
+          inarow=0;}
+        else{
+          inarow++;}
+      }
+    }
+    return toreturn;
+  }
+
+  public ArrayList<Integer> checkCols(){
+    int currentcolor;
+    int candycolor;
+    int inarow;
+    ArrayList<Integer> toreturn = new ArrayList<Integer>();
+    for (int a=0;a<row;a++){
+      currentcolor=0;
+      candycolor=0;
+      inarow=0;
+      for (int b=0;b<col;b++){
+        candycolor=candyGrid[b][a].getColorInt();
+        if (candycolor!=currentcolor) {
+          currentcolor=candycolor;
+          if (inarow>=3){
+            toreturn.add(b);}
+          inarow=0;}
+        else{
+          inarow++;}
+      }
+    }
+    return toreturn;}
+
+
 }
