5cdcb03dd85b071876274000cdbab470badea262
aditihaiman <ahaiman00@stuy.edu>
2019-01-21 13:00:02 -0500

    updated CandyCrush

--- CandyCrush.java
+++ CandyCrush.java
@@ -15,9 +15,14 @@ import com.googlecode.lanterna.input.KeyMappingProfile;
 import com.googlecode.lanterna.screen.Screen;
 import java.io.*;
 import java.util.*;
+/*remaining bugs:
+bottom row and first column still spaz
+has stopped catching certain column matches that go into bottom row
+doesn't always catch power-ups
+generally spazzy
+*/
 
-
-public class CandyCrush {
+public class CandyCrush{
 
   public static void putString(int r, int c,Terminal t, String s){
       t.applyBackgroundColor(Terminal.Color.DEFAULT);
@@ -42,8 +47,22 @@ public class CandyCrush {
     terminal.setCursorVisible(false);
     putString(0,0,terminal,"WELCOME TO CANDY CRUSH!",Terminal.Color.GREEN,Terminal.Color.WHITE);
     putString(0,1,terminal,"To quit, press escape.");
-    putString(0,2, terminal, "Choose a difficulty to start: 1,2 or 3");
+    putString(0,2,terminal,"To view instructions, press \"I\"");
+    putString(0,3, terminal, "Choose a difficulty to start: 1, 2, or 3");
     terminal.moveCursor(10,10);
+  }
+
+//Setup for instructions screens
+  public static void setupInstructions(Terminal t){
+    t.setCursorVisible(false);
+    putString(0,0,t,"Press backspace to go back to the menu screen");
+    putString(0,2,t,"The goal of Candy Crush is to crush candies by connecting 3 or\nmore candies of "+
+    "the same color vertically or horizontally. By\ncrushing candies, you can gain points. Try to reach the "+
+    "\nobjective number of points before you run out of moves. You \ncan swipe candies around by first selecting them. "+
+    "Move using \nthe arrow keys to the candy you would like to select, then \npress enter to select it. "+
+    "Once you have selected a candy, use \nthe arrow keys again to indicate which direction you would " +
+    "\nlike to swipe it in. If you get more than 3 candies in a row, \nyou will create a super candy. These "+
+    "super candies can allow \nyou to clear a whole row or column of candies!");
   }
 
 //Setup after a level has been chosen
@@ -74,7 +93,9 @@ public class CandyCrush {
           if (c==3) t.applyForegroundColor(Terminal.Color.GREEN);
           if (c==4) t.applyForegroundColor(Terminal.Color.WHITE);
           if (c==5) t.applyForegroundColor(Terminal.Color.MAGENTA);
-          t.putCharacter('O');}
+          if (a.getGrid()[i][b].getSpecial()) t.putCharacter('\u25CB');
+          else {t.putCharacter('\u25CF');}
+        }
         x++;
       }
       x=x1;
@@ -92,7 +113,7 @@ public class CandyCrush {
       for(int a = 0; a < rows.get(2); a++) {
         t.moveCursor(x+rows.get(1), y+rows.get(0));
         t.applyBackgroundColor(Terminal.Color.CYAN);
-        t.putCharacter('O');
+        t.putCharacter('\u25CF');
         x++;
       }
     }
@@ -108,7 +129,7 @@ public class CandyCrush {
       for(int a = 0; a < cols.get(2); a++) {
         t.moveCursor(x+cols.get(1), y+cols.get(0));
         t.applyBackgroundColor(Terminal.Color.CYAN);
-        t.putCharacter('O');
+        t.putCharacter('\u25CF');
         y++;
       }
     }
@@ -123,17 +144,18 @@ public class CandyCrush {
       highlightRow(test, t);
       Thread.sleep(500); //delays so that you can see what is happening in the grid
       runs=test.popRows(); //crushes rows
-      printpuzzle(test, x1, y1, t, move);
-      Thread.sleep(500); //delay
-      test.fillEmptyGrid();
-      printpuzzle(test, x1, y1, t, move);
+      if (runs){
+        printpuzzle(test, x1, y1, t, move);
+        Thread.sleep(500); //delay
+        test.fillEmptyGrid();
+        printpuzzle(test, x1, y1, t, move);}
       highlightCol(test, t);
       Thread.sleep(500); //delay
       run=test.popCols(); //crushes columns
-      printpuzzle(test, x1, y1, t, move);
-      Thread.sleep(500); //delay
-      test.fillEmptyGrid();
-      printpuzzle(test, x1, y1, t, move);
+      if (run){
+        printpuzzle(test, x1, y1, t, move);
+        test.fillEmptyGrid();
+        printpuzzle(test, x1, y1, t, move);}
     }
   }
 
@@ -149,7 +171,7 @@ public class CandyCrush {
     boolean running = true;
     String mode = "SETUP"; //determines the mode
     int moves = 10; //number of moves
-    int objective=1000; //points objective
+    int objective=800; //points objective
     int x = 10; int y = 10;//starting point for where to print the grid
 
     CandyGrid tester= new CandyGrid(10);//creates new puzzle
@@ -170,45 +192,55 @@ public class CandyCrush {
 
         if (key.getKind() == Key.Kind.Escape) {
           terminal.exitPrivateMode();
-          running = false;
-        }
+          running = false;}
 
         if (mode.equals("SETUP")){ //setup screen
           terminal.setCursorVisible(false);
           if (key!=null){
+            if (key.getCharacter()=='i'){
+              terminal.clearScreen();
+              mode="INSTRUCTIONS";
+            }
+            objective=800;
             if (key.getCharacter()=='1'){ //triggers setup for different levels
               terminal.clearScreen();
               tester= new CandyGrid(10);//creates new puzzle
               moves = 10;
-              objective=1000;
               setupGame(terminal, tester, moves, objective);
               popDelay(tester, 10, 10, terminal, moves);
               mode="GAME";
-              //terminal.moveCursor(10, 10);
             }
             if (key.getCharacter()=='2'){
               terminal.clearScreen();
               tester= new CandyGrid(12);//creates new puzzle
               moves = 8;
-              objective=1000;
               setupGame(terminal, tester, moves, objective);
               popDelay(tester, 10, 10, terminal, moves);
               mode="GAME";
-              //terminal.moveCursor(10, 10);
             }
             if (key.getCharacter()=='3'){
               terminal.clearScreen();
-              tester=new CandyGrid(13);
+              tester=new CandyGrid(15);
               moves = 6;
-              objective=1000;
               setupGame(terminal, tester, moves, objective);
               popDelay(tester, 10, 10, terminal, moves);
               mode="GAME";
-              //terminal.moveCursor(10, 10);
             }
             terminal.moveCursor(10, 10);
             x=10;
             y=10;
+          }
+        }
+
+        if(mode.equals("INSTRUCTIONS")){
+          terminal.setCursorVisible(false);
+          setupInstructions(terminal);
+          if(key!=null){
+            if(key.getKind()==Key.Kind.Backspace) {
+              terminal.clearScreen();
+              mode="SETUP";
+              setupMenu(terminal);
+            }
           }
         }
 
@@ -256,50 +288,46 @@ public class CandyCrush {
           terminal.setCursorVisible(true);
           if (key!=null){
             int beforex, beforey;
-            if (key.getKind()==Key.Kind.ArrowLeft){ //candies are switched in the specified direction, then matching candies are crushed
+            if (key.getKind()==Key.Kind.ArrowLeft && x>10){ //candies are switched in the specified direction, then matching candies are crushed
               beforex=x;
               beforey=y;
               tester.swipeCandies(y-10,x-10,"HORIZONTAL",1);
               terminal.setCursorVisible(false);
               popDelay(tester, 10, 10, terminal, moves);
               moves--;
-              printpuzzle(tester, 10, 10, terminal, moves);
               terminal.setCursorVisible(true);
               terminal.moveCursor(beforex,beforey);
               mode="GAME";
             }
-            if (key.getKind()==Key.Kind.ArrowRight){
+            if (key.getKind()==Key.Kind.ArrowRight && x<9+tester.getCol()){
               beforex=x;
               beforey=y;
               tester.swipeCandies(y-10,x-10,"HORIZONTAL",-1);
               terminal.setCursorVisible(false);
               popDelay(tester, 10, 10, terminal, moves);
               moves--;
-              printpuzzle(tester, 10, 10, terminal, moves);
               terminal.setCursorVisible(true);
               terminal.moveCursor(beforex,beforey);
               mode="GAME";
             }
-            if (key.getKind()==Key.Kind.ArrowUp){
+            if (key.getKind()==Key.Kind.ArrowUp && y>10){
               beforex=x;
               beforey=y;
               tester.swipeCandies(y-10,x-10,"VERTICAL",1);
               terminal.setCursorVisible(false);
               popDelay(tester, 10, 10, terminal, moves);
               moves--;
-              printpuzzle(tester, 10, 10, terminal, moves);
               terminal.setCursorVisible(true);
               terminal.moveCursor(beforex,beforey);
               mode="GAME";
             }
-            if (key.getKind()==Key.Kind.ArrowDown){
+            if (key.getKind()==Key.Kind.ArrowDown && y<9+tester.getRow()){
               beforex=x;
               beforey=y;
               tester.swipeCandies(y-10,x-10,"VERTICAL",-1);
               terminal.setCursorVisible(false);
               popDelay(tester, 10, 10, terminal, moves);
               moves--;
-              printpuzzle(tester, 10, 10, terminal, moves);
               terminal.setCursorVisible(true);
               terminal.moveCursor(beforex,beforey);
               mode="GAME";
@@ -318,8 +346,7 @@ public class CandyCrush {
         if(mode.equals("LOSE")){ //lose screen
           terminal.setCursorVisible(false);
           terminal.clearScreen();
-          terminal.applySGR(Terminal.SGR.ENTER_BOLD,Terminal.SGR.ENTER_BLINK);
-          putString(5,10,terminal, "You lost. Press backspace to return to the menu screen or escape to exit.",Terminal.Color.RED,Terminal.Color.WHITE);
+          putString(0,10,terminal, "You lost. Press backspace to return to the menu screen or escape to exit.",Terminal.Color.RED,Terminal.Color.WHITE);
           terminal.applySGR(Terminal.SGR.RESET_ALL);
           if (key!= null && key.getKind() == Key.Kind.Escape) { //to exit screen
             terminal.clearScreen();
@@ -338,7 +365,7 @@ public class CandyCrush {
           terminal.setCursorVisible(false);
           terminal.clearScreen();
           terminal.applySGR(Terminal.SGR.ENTER_BOLD,Terminal.SGR.ENTER_BLINK);
-          putString(5, 10, terminal, "CONGRATULATIONS, YOU WON! Press backspace to return to the menu screen or escape to exit.",Terminal.Color.GREEN,Terminal.Color.WHITE);
+          putString(0, 10, terminal, "CONGRATULATIONS, YOU WON! Press backspace to return to the menu screen or escape to exit.",Terminal.Color.GREEN,Terminal.Color.WHITE);
           terminal.applySGR(Terminal.SGR.RESET_ALL);
           if (key!= null && key.getKind() == Key.Kind.Escape) { //to exit screen
             terminal.clearScreen();
